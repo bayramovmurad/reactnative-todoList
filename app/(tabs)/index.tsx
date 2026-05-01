10f698d8eaa2d714729ac6508ccf8f211e3b5426
bayramovmurad <demonseytandir@gmail.com>
2026-05-02 00:24:41 +0300

    feat(backend) created database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,28 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useMutation, useQuery } from "convex/react";
+import { Text, View, StyleSheet, TouchableOpacity, FlatList } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+  const getTodos = useQuery(api.todos.getTodos);
+  console.log(getTodos);
+  
+  const addTodo = useMutation(api.todos.addTodo);
+
   return (
     <View style={styles.container}>
       <Text>salam</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({text:"im done"})}>
+        <Text>Add new todo</Text>
+      </TouchableOpacity>
+      <FlatList 
+      data={getTodos}
+      renderItem={(item) => <Text>{}</Text>}
+      />
     </View>
   );
 }
@@ -16,12 +30,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center", // ortada
+    justifyContent: "center", 
     alignItems: "center",
   },
   title: {
     color: "red",
-    fontSize: 20, // burada olmalıdır
+    fontSize: 20,
     textAlign: "center",
     fontWeight: 700,
   },
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,21 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 export default function RootLayout() {
 
+  const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+  });
+
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
-      </Stack>;
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
+        </Stack>
+        ;
+      </ThemeProvider>
+    </ConvexProvider>
   );
 };
