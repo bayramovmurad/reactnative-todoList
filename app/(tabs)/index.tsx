4bf4c54f7a21b7c77114b12ce918a5e4b2a1b6f5
bayramovmurad <demonseytandir@gmail.com>
2026-05-03 13:59:23 +0300

    feat(fullstack): created todo input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import { Text, TouchableOpacity, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {LinearGradient} from "expo-linear-gradient";
 import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
@@ -19,6 +20,7 @@ export default function Index() {
       <StatusBar barStyle={colors.statusBarStyle}/>
       <SafeAreaView style={homeStyles.safeArea}>
        <Header/>
+       <TodoInput/>
         <TouchableOpacity onPress={toggleDarkMode}>
           <Text>toggle the mode</Text>
         </TouchableOpacity>
